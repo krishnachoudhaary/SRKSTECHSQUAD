027cb83f615396cec053047380e89377a87cb17f
Krishna Kumar <krishnachoudhary0609@gmail.com>
2026-09-18 12:57:35 +0530

    feat(routes): register full CRUD and cancellation routes in booking_routes

--- server/routes/booking_routes.py
+++ server/routes/booking_routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
-from controllers.booking_controller import create_booking, get_user_bookings
+from controllers.booking_controller import create_booking, get_user_bookings, get_booking_by_id, cancel_booking
 from middleware.auth_middleware import token_required
 
 booking_bp = Blueprint('booking_bp', __name__)
 
 booking_bp.route('', methods=['POST'])(token_required(create_booking))
+booking_bp.route('', methods=['GET'])(token_required(get_user_bookings))
 booking_bp.route('/my', methods=['GET'])(token_required(get_user_bookings))
+booking_bp.route('/<int:booking_id>', methods=['GET'])(token_required(get_booking_by_id))
+booking_bp.route('/<int:booking_id>/cancel', methods=['POST'])(token_required(cancel_booking))
